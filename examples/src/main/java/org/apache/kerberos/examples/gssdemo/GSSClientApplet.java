5778ba08b780a7ab60926f308b4972a915782186
Michael E. Rodriguez <erodriguez@apache.org>
2007-05-14 06:12:14 +0000

    Updates to old GSS example code: o  Reformat. o  Organize imports.
    git-svn-id: https://svn.apache.org/repos/asf/directory/clients/trunk/kerberos@537722 13f79535-47bb-0310-9956-ffa450edef68

--- examples/src/main/java/org/apache/kerberos/examples/gssdemo/GSSClientApplet.java
+++ examples/src/main/java/org/apache/kerberos/examples/gssdemo/GSSClientApplet.java
@@ -16,6 +16,7 @@
  */
 package org.apache.kerberos.examples.gssdemo;
 
+
 import java.applet.Applet;
 import java.awt.Button;
 import java.awt.Color;
@@ -26,102 +27,104 @@ import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class GSSClientApplet extends Applet
 {
+    private GSSClient gssClient = null;
 
-	private GSSClient gssClient = null;
+    // UI parameters
+    private Label lblUserName = new Label( "Username:" );
+    private Label lblPassword = new Label( "Password:" );
 
-	// UI parameters
-	private Label lblUserName = new Label("Username:");
-	private Label lblPassword = new Label("Password:");
+    private TextField tfUserName = new TextField( 12 );
+    private TextField tfPassword = new TextField( 12 );
 
-	private TextField tfUserName = new TextField(12);
-	private TextField tfPassword = new TextField(12);
+    private Button buttonPartner1 = new Button( "  Login to ldap  " );
 
-	private Button buttonPartner1 = new Button("  Login to ldap  ");
+    private Color bgColor = new Color( 204, 204, 255 );
 
-	private Color bgColor = new Color(204, 204, 255);
+    private TextArea taResponse = null;
 
-	private TextArea taResponse = null;
+    // GSS parameters.
+    private String remotePeer = null;
+    private String kerberosRealm = null;
+    private String kdcAddress = null;
+    private String addressOfRemotePeer = null;
+    private int portOfRemotePeer;
 
-	// GSS parameters.
-	private String remotePeer = null;
-	private String kerberosRealm = null;
-	private String kdcAddress = null;
-	private String addressOfRemotePeer = null;
-	private int portOfRemotePeer;
 
-	public void init()
+    public void init()
     {
-		setLayout(new FlowLayout(FlowLayout.CENTER));
-		add(lblUserName);
-		add(tfUserName);
-		add(lblPassword);
-		add(tfPassword);
+        setLayout( new FlowLayout( FlowLayout.CENTER ) );
+        add( lblUserName );
+        add( tfUserName );
+        add( lblPassword );
+        add( tfPassword );
 
-		buttonPartner1.setBackground(bgColor);
+        buttonPartner1.setBackground( bgColor );
 
-		kerberosRealm       = "25OZ.COM";
-		kdcAddress          = "localhost";
-		addressOfRemotePeer = "localhost";
+        kerberosRealm = "25OZ.COM";
+        kdcAddress = "localhost";
+        addressOfRemotePeer = "localhost";
 
-		add(buttonPartner1);
-		buttonPartner1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent evt) {
-				remotePeer = "ldap";
-				portOfRemotePeer = 1082;
-				login();
-			}
-		});
-
-		taResponse = new TextArea("[Output Window] ...\n\r", 12, 58);
-		taResponse.setBackground(Color.white);
-		add(taResponse);
-	}
-
-	private void login()
-    {
-		try
+        add( buttonPartner1 );
+        buttonPartner1.addActionListener( new ActionListener()
         {
-			if (tfUserName.getText().equals("") && tfPassword.getText().equals(""))
-				taResponse.append("Please use your username to login ...\n\r");
-			else
+            public void actionPerformed( ActionEvent evt )
             {
-				gssClient = new GSSClient(tfUserName.getText() + "@" + kerberosRealm,
-						tfPassword.getText(), remotePeer, addressOfRemotePeer,
-						portOfRemotePeer, kerberosRealm, kdcAddress);
+                remotePeer = "ldap";
+                portOfRemotePeer = 1082;
+                login();
+            }
+        } );
 
-				taResponse.append(tfUserName.getText() + " being logged in ...\n\r");
-				
-				gssClient.login();
-				
-				if (gssClient.hasConfidentialContext())
+        taResponse = new TextArea( "[Output Window] ...\n\r", 12, 58 );
+        taResponse.setBackground( Color.white );
+        add( taResponse );
+    }
+
+
+    private void login()
+    {
+        try
+        {
+            if ( tfUserName.getText().equals( "" ) && tfPassword.getText().equals( "" ) )
+                taResponse.append( "Please use your username to login ...\n\r" );
+            else
+            {
+                gssClient = new GSSClient( tfUserName.getText() + "@" + kerberosRealm, tfPassword.getText(),
+                    remotePeer, addressOfRemotePeer, portOfRemotePeer, kerberosRealm, kdcAddress );
+
+                taResponse.append( tfUserName.getText() + " being logged in ...\n\r" );
+
+                gssClient.login();
+
+                if ( gssClient.hasConfidentialContext() )
                 {
-					String message = new String("Sample secret message from client");
-					taResponse.append("You are successfully logged in ... \n\r");
-					taResponse.append("Sending [" + message + "] to server \n\r");
-					String response = gssClient.sendMessageReturnReply(message);
-					taResponse.append("Server response ... " + response + "\n\r");
-				}
+                    String message = new String( "Sample secret message from client" );
+                    taResponse.append( "You are successfully logged in ... \n\r" );
+                    taResponse.append( "Sending [" + message + "] to server \n\r" );
+                    String response = gssClient.sendMessageReturnReply( message );
+                    taResponse.append( "Server response ... " + response + "\n\r" );
+                }
                 else
                 {
-					taResponse.append("Confidential context failed. \n\r");
-				}
+                    taResponse.append( "Confidential context failed. \n\r" );
+                }
 
-				try
+                try
                 {
-					gssClient.logout();
-				}
-                catch (Exception e)
+                    gssClient.logout();
+                }
+                catch ( Exception e )
                 {
-					e.printStackTrace();
-				}
-			}
-		}
-        catch (Exception e)
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch ( Exception e )
         {
-			taResponse.append("Exception ..." + e.getMessage() + "\n\r");
-		}
-	}
+            taResponse.append( "Exception ..." + e.getMessage() + "\n\r" );
+        }
+    }
 }
-
