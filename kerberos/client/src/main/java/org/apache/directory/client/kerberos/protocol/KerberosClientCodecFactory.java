b3238d2a01df69ed9fce9ad56a3d6299bdab7ff6
Michael E. Rodriguez <erodriguez@apache.org>
2007-05-26 07:22:58 +0000

    First pass at Kerberos client: o  Does retrieve ticket-granting tickets (TGT). o  Does not support any configuration.
    git-svn-id: https://svn.apache.org/repos/asf/directory/clients/trunk@541868 13f79535-47bb-0310-9956-ffa450edef68

---
+++ kerberos/client/src/main/java/org/apache/directory/client/kerberos/protocol/KerberosClientCodecFactory.java
@@ -0,0 +1,66 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.client.kerberos.protocol;
+
+
+import org.apache.mina.filter.codec.ProtocolCodecFactory;
+import org.apache.mina.filter.codec.ProtocolDecoder;
+import org.apache.mina.filter.codec.ProtocolEncoder;
+
+
+/**
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev: 540371 $, $Date: 2007-05-21 17:00:43 -0700 (Mon, 21 May 2007) $
+ */
+public class KerberosClientCodecFactory implements ProtocolCodecFactory
+{
+    private static final KerberosClientCodecFactory INSTANCE = new KerberosClientCodecFactory();
+
+
+    /**
+     * Returns the singleton {@link KerberosClientCodecFactory}.
+     *
+     * @return The singleton {@link KerberosClientCodecFactory}.
+     */
+    public static KerberosClientCodecFactory getInstance()
+    {
+        return INSTANCE;
+    }
+
+
+    private KerberosClientCodecFactory()
+    {
+        // Private constructor prevents instantiation outside this class.
+    }
+
+
+    public ProtocolEncoder getEncoder()
+    {
+        // Create a new encoder.
+        return new KerberosClientEncoder();
+    }
+
+
+    public ProtocolDecoder getDecoder()
+    {
+        // Create a new decoder.
+        return new KerberosClientDecoder();
+    }
+}
